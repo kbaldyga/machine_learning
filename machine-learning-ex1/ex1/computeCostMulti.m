b15f62cb5dd4a850bc24641ccceced9218e66acf
Kamil Baldyga <kbaldyga@gmail.com>
2017-10-23 16:22:19 +0700

    part 2

--- machine-learning-ex1/ex1/computeCostMulti.m
+++ machine-learning-ex1/ex1/computeCostMulti.m
@@ -14,7 +14,8 @@ J = 0;
 %               You should set J to the cost.
 
 
-
+prediction = X * theta;
+J=1/(2*m)*sum((prediction-y) .^ 2);
 
 
 % =========================================================================
